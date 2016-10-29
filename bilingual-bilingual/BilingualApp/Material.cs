d2c3acc51643cfbad87b7989d064d4acad546877
jaime adrian magaña vargas <jasonvorhiskill@gmail.com>
2016-10-29 17:51:25 -0500

    10/292016

--- bilingual-bilingual/BilingualApp/Material.cs
+++ bilingual-bilingual/BilingualApp/Material.cs
@@ -31,5 +31,38 @@ namespace BilingualApp
             bi.Visible = true;
             Close();
         }
+
+        private void VerboToBe_CheckedChanged(object sender, EventArgs e)
+        {
+            if(VerboToBe.Checked=false)
+            {
+                Nivel1vtb.Enabled = false;
+                nivel2vtb.Enabled = false;
+                Nivel3vtb.Enabled = false;
+            }
+        }
+
+        private void DoWhile_CheckedChanged(object sender, EventArgs e)
+        {
+            if(DoWhile.Checked=false)
+            {
+                Nivel1dw.Enabled = false;
+                Nivel2dw.Enabled = false;
+                Nivel3dw.Enabled = false;
+            }
+        }
+
+        private void Gramatica_CheckedChanged(object sender, EventArgs e)
+        {
+            if(Gramatica.Checked=false)
+            {
+                grama.Enabled = false;
+            }
+        }
+
+        private void Nivel1vtb_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

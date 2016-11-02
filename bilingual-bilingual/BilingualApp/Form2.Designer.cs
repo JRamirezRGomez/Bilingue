410a1eb1547f5363faec97f447cf0067b08064a4
Jose Raul Ramirez Gomez <ramirez.gomez1995@gmail.com>
2016-11-01 18:17:46 -0600

    wisiwwiiiiii

---
+++ bilingual-bilingual/BilingualApp/Form2.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace BilingualApp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn
+            // 
+            this.Btn.Location = new System.Drawing.Point(97, 119);
+            this.Btn.Name = "Btn";
+            this.Btn.Size = new System.Drawing.Size(75, 23);
+            this.Btn.TabIndex = 0;
+            this.Btn.Text = "button1";
+            this.Btn.UseVisualStyleBackColor = true;
+            this.Btn.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Btn);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn;
+    }
+}

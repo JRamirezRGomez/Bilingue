bd0ed0511e997bb9d7e004e3c88cc657531fb07f
Jose Raul Ramirez Gomez <ramirez.gomez1995@gmail.com>
2016-11-10 16:54:10 -0600

    cambios minimos

---
+++ bilingual-bilingual/BilingualApp/ConexionBD.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace BilingualApp
+{
+    public partial class ConexionBD : Form
+    {
+        public ConexionBD()
+        {
+            InitializeComponent();
+
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = "localhost";
+            builder.UserID = "root";
+            builder.Password = "";
+            builder.Database = "Bilingual";
+        }
+
+        
+    }
+}

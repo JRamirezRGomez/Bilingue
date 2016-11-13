129c5121fc67de59d05c002176c785181b9d8137
jaime adrian magaña vargas <jasonvorhiskill@gmail.com>
2016-11-12 19:04:52 -0600

    prueba de fondos nada aun seguro

--- bilingual-bilingual/BilingualApp/PreguntasVERBTOBENvl1.Designer.cs
+++ bilingual-bilingual/BilingualApp/PreguntasVERBTOBENvl1.Designer.cs
@@ -31,64 +31,79 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PreguntasVERBTOBENvl1));
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.OpcionA = new System.Windows.Forms.LinkLabel();
+            this.OpcionB = new System.Windows.Forms.LinkLabel();
+            this.OpcionC = new System.Windows.Forms.LinkLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(-4, 12);
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.richTextBox1.DetectUrls = false;
+            this.richTextBox1.Location = new System.Drawing.Point(-1, -1);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(471, 243);
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(468, 256);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(465, 12);
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(465, -1);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(271, 243);
+            this.pictureBox1.Size = new System.Drawing.Size(278, 256);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
-            // label1
+            // OpcionA
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 303);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(16, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "1)";
+            this.OpcionA.AutoSize = true;
+            this.OpcionA.BackColor = System.Drawing.Color.Transparent;
+            this.OpcionA.LinkColor = System.Drawing.Color.Red;
+            this.OpcionA.Location = new System.Drawing.Point(11, 311);
+            this.OpcionA.Name = "OpcionA";
+            this.OpcionA.Size = new System.Drawing.Size(51, 13);
+            this.OpcionA.TabIndex = 2;
+            this.OpcionA.TabStop = true;
+            this.OpcionA.Text = "Opcion A";
             // 
-            // label2
+            // OpcionB
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 374);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(16, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "2)";
+            this.OpcionB.AutoSize = true;
+            this.OpcionB.BackColor = System.Drawing.Color.Transparent;
+            this.OpcionB.LinkColor = System.Drawing.Color.Red;
+            this.OpcionB.Location = new System.Drawing.Point(11, 366);
+            this.OpcionB.Name = "OpcionB";
+            this.OpcionB.Size = new System.Drawing.Size(51, 13);
+            this.OpcionB.TabIndex = 3;
+            this.OpcionB.TabStop = true;
+            this.OpcionB.Text = "Opcion B";
             // 
-            // label3
+            // OpcionC
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 440);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(16, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "3)";
+            this.OpcionC.AutoSize = true;
+            this.OpcionC.BackColor = System.Drawing.Color.Transparent;
+            this.OpcionC.LinkColor = System.Drawing.Color.Red;
+            this.OpcionC.Location = new System.Drawing.Point(12, 418);
+            this.OpcionC.Name = "OpcionC";
+            this.OpcionC.Size = new System.Drawing.Size(51, 13);
+            this.OpcionC.TabIndex = 4;
+            this.OpcionC.TabStop = true;
+            this.OpcionC.Text = "Opcion C";
             // 
             // PreguntasVERBTOBENvl1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::BilingualApp.Properties.Resources.imagesM3H78NUU;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(742, 492);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OpcionC);
+            this.Controls.Add(this.OpcionB);
+            this.Controls.Add(this.OpcionA);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -104,8 +119,8 @@
 
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel OpcionA;
+        private System.Windows.Forms.LinkLabel OpcionB;
+        private System.Windows.Forms.LinkLabel OpcionC;
     }
 }
